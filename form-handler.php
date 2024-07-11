ad5fbc70934a45148880c528c97679b4a331cea1
Angelconsultant <angelmeche29@gmail.com>
2024-07-11 13:11:21 +0100

    fix some issue
    make Changes

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'http://dekingsyardglobalventures-ltd.vercel.app'
+$email_from = 'https://dekinsyardglobalventures-ltd.vercel.app/'
 
 $email_subject = 'new form submission'
 
