729988f6381b4dbd79c90d6e56f6e7356234803e
Angelconsultant <angelmeche29@gmail.com>
2024-07-11 09:44:18 +0100

    first commit

---
+++ form-handler.php
@@ -0,0 +1,33 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'dekingsyardwebsite.com'
+
+$email_subject = 'new form submission'
+
+$email_body = "User Name: $name.\n".
+              "User Email:$visitor_email.\n".
+               "Subject: $subject.\n".
+               "User Message: $message.\n".;
+
+ $to = 'dekingsyardglobalventurslimit@gmail.com';
+
+ $headers = "From: $email_from \r\n";   
+ 
+ $header = "Reoly-To: $visitor_email \r\n";
+
+ mail($to,$email_subject,$email_body,$headers);
+
+ header("Location: contact.html")
+
+
+
+
+
+
+
+
+?>
